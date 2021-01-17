9c9f1aa54839e6e3845cc8f9af26971a8eba35fe
Fernando Ochoa <Fernando268a@gmail.com>
2021-01-16 19:08:47 -0500

    borrar archivos

--- Licencia.js
+++ Licencia.js
@@ -3,64 +3,22 @@ const password = document.getElementById("password");
 const licencia = document.getElementById("lisencia");
 const form = document.querySelector('form');
 const text = document.getElementById("text");
+const parrafo = document.getElementsByName('parrafo')
+var entrar = false
 
-form.addEventListener("submit",(event) => {
-    event.preventDefault();
-    let p = "No es valido tu ";
-    let entrar = false;
-    let en = false;
-    if (licencia.value.length < 5) {
-        p += "licencia "
-        entrar = true;
-        if(password.value.length > 7){
-            en = true;
-        }
-    }
 
-    if(user.value.length < 6){
-        if (entrar) {
-            if(en){
-                p += " y usario";
-            }else{
-                p += " ,usario";
-            }
-        }else{
-            p += " usario";
-        }
-        
-        entrar = true;
-    }
-    if(password.value.length < 7){
-        if (entrar) {
-            p += " y password.";
-        }else{
-            p += " password";
-        }
-        entrar = true;
-    }
+function validacion(Letras,nbj){
 
-    if(entrar){
-        swal('Registro',p,'error')
-        licencia.value = "";
-        user.value = "";
-        password.value = "";
+    if (h >= Letras) {
+        parrafo[nbj].innnerHTML += '*no valido';
     } else {
-        text.innerHTML = `enviado`;
-        const $forData = new FormData(form)
-        const lic = $forData.get("licencia")
-        const username = $forData.get("usario")
-        const motDePass = $forData.get("password")
-
-        fetch('/login',{
-        method: 'POST',
-        body: $forData
-        })
+        parrafo[nbj].innnerHTML += 'no'
+        entrar = true
     }
+}
 
-    
-
+form.addEventListener('submit', (eve) => {
+    eve.preventDefault();
+    validacion(licencia,5,0)
 })
-
-
-
 
