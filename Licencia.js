56a1e8217a36d2aed2336d16c64d47626e2f7078
Fernando Ochoa <Fernando268a@gmail.com>
2021-01-17 11:03:28 -0500

    modificacion

--- Licencia.js
+++ Licencia.js
@@ -1,24 +1,22 @@
 const user = document.getElementById("user");
 const password = document.getElementById("password");
-const licencia = document.getElementById("lisencia");
+const licencia = document.querySelector("lisencia");
 const form = document.querySelector('form');
 const text = document.getElementById("text");
 const parrafo = document.getElementsByName('parrafo')
 var entrar = false
 
 
-function validacion(Letras,nbj){
-
-    if (h >= Letras) {
-        parrafo[nbj].innnerHTML += '*no valido';
+function validacion(o){
+    if (0 === 5) {
+        console.log('it works');
     } else {
-        parrafo[nbj].innnerHTML += 'no'
-        entrar = true
+        console.log('it ')
     }
 }
 
 form.addEventListener('submit', (eve) => {
     eve.preventDefault();
-    validacion(licencia,5,0)
+    validacion(licencia)
 })
 
